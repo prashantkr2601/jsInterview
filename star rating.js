13e1c009200d6e7bafc4d9850d7f701eb64789be
Prashant kumar <prashantkr2601@gmail.com>
2024-09-02 03:26:14 +0530

    Create star rating React.js

---
+++ star rating.js
@@ -0,0 +1,30 @@
+function RatingReview({ rating, setRating }) {
+  return (
+    <div>
+      {[1, 2, 3, 4, 5].map((star) => {
+        return (
+          <span
+            className="start"
+            style={{
+              cursor: "pointer",
+              color: rating >= star ? "gold" : "gray",
+              fontSize: `35px`,
+            }}
+            onClick={() => {
+              setRating(star);
+            }}
+          >
+            {" "}
+            ★{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+
+function App() {
+  const [rating, setRating] = useState(0);
+  return <RatingReview rating={rating} setRating={setRating} />;
+}
+export default App;
